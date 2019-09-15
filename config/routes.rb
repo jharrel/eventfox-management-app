737f468068f374db13e8940ec71994007d7048c5
jharrel <harrel.jason@gmail.com>
2019-09-15 13:48:53 -0700

    Added Views models controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,32 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
   root "static#welcome"
 
+  resources :sessions
+
+  resources :users, only: [:new, :create] do
+    resources :tasks, only: [:index, :show, :new]
+  end
+
+  resources :projects
+
+  resources :tasks
+
+
+  get '/signout', to: 'sessions#destroy'
+
+  get '/tasks/:id/complete', to: "tasks#complete"
+
+  #get 'tasks/:id/delete', to: "tasks#delete"
+
+  get '/auth/facebook/callback' => 'sessions#create'
+
+  # get 'projects/:id/delete', to: "projects#delete"
+
+  #get '/users/most_projects', to: "users#most_projects"
 end
