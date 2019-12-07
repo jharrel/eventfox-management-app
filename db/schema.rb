2f55d763e03a442b36a4edebbd8bd76d9b882c3c
jharrel <harrel.jason@gmail.com>
2019-12-06 22:25:51 -0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_02_062246) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
